1be9d34686b4f4ef0b8e6c5027b4f57edccb8d13
Rob Jahn <robjahn@gmail.com>
2026-01-12 16:32:33 -0500

    Add print statements for additional environment variables

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import os
 
 print("hello world")
-print("TEST_VAR1 = " + os.getenv("TEST_VAR1", ""))
-print("TEST_VAR2 = " + os.getenv("TEST_VAR2", ""))
-print("TEST_VAR3 = " + os.getenv("TEST_VAR3", ""))
+print("TEST_VAR10 = " + os.getenv("TEST_VAR10", ""))
+print("TEST_VAR20 = " + os.getenv("TEST_VAR20", ""))
+print("TEST_VAR30 = " + os.getenv("TEST_VAR30", ""))
+print("TEST_VAR40 = " + os.getenv("TEST_VAR40", ""))
